094317bd8f4b4a76375b837e25bac3c3faae5f7b
silence <867943221@qq.com>
2021-06-29 18:04:01 +0800

    init

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -25,7 +25,8 @@
   import {Component, Prop} from 'vue-property-decorator';
   @Component
   export default class NumberPad extends Vue {
-    output: string = '0';
+    @Prop() readonly value!: number;
+    output = this.value.toString();
     inputContent(event: MouseEvent) {
       const button = (event.target as HTMLButtonElement);
       const input = button.textContent!;
@@ -52,6 +53,9 @@
       this.output = '0';
     }
     ok() {
+      this.$emit('update:value', this.output);
+      this.$emit('submit', this.output);
+      this.output = '0';
     }
   }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,21 +1,52 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types xxx="hi"/>
-    <Notes/>
-    <Tags/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
 <script lang="ts">
+  import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  export default {
-    name: 'Money',
-    components: {Tags, Notes, Types, NumberPad},
-  };
+  import {Component, Watch} from 'vue-property-decorator';
+  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+  type Record = {
+    tags: string[]
+    notes: string
+    type: string
+    amount: number // 数据类型 object | string
+    createdAt?: Date  // 类 / 构造函数
+  }
+  @Component({
+    components: {Tags, Notes, Types, NumberPad}
+  })
+  export default class Money extends Vue {
+    tags = ['衣', '食', '住', '行', '彩票'];
+    recordList: Record[] = recordList;
+    record: Record = {
+      tags: [], notes: '', type: '-', amount: 0
+    };
+    onUpdateTags(value: string[]) {
+      this.record.tags = value;
+    }
+    onUpdateNotes(value: string) {
+      this.record.notes = value;
+    }
+    saveRecord() {
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createdAt = new Date();
+      this.recordList.push(record2);
+    }
+    @Watch('recordList')
+    onRecordListChange() {
+      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    }
+  }
 </script>
 
 <style lang="scss">
