49e306213aba215fc9381691b2b320067e7abc40
silence <867943221@qq.com>
2021-07-02 16:32:46 +0800

    init

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,23 +1,16 @@
 <template>
-  <svg class="icon">
-         <use :xlink:href="'#'+name"/>
-     </svg>
-
+  <svg class="icon" @click="$emit('click', $event)">
+    <use :xlink:href="'#'+name"/>
+  </svg>
 </template>
 
 <script lang="ts">
-
-    let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try{
-importAll(require.context( '../assets/icons', true,/\.svg$/));
-} catch (error) {
-    console.log(error);
-}
-
-    export default {
-        props: ['name'],
-       name: 'Icon' 
-    }
+  let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+  export default {
+    props: ['name'],
+    name: 'Icon'
+  };
 </script>
 
 <style lang="scss" scoped>
